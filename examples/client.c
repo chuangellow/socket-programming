91c4b0d04056994cee94c42250fe27ee5aa32152
Techuang <chuangellow@gmail.com>
2023-12-11 19:12:48 +0800

    Update on the example for client and server

--- examples/client.c
+++ examples/client.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
-#include<sys/socket.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main(void) {
+	int sock_fd;
+	if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	struct sockaddr_in server_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(PORT),
+	};
+
+	if (connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+		perror("connect");
+		exit(EXIT_FAILURE);
+	}
+	return 0;
+}
--- examples/server.c
+++ examples/server.c
@@ -9,10 +9,32 @@
 
 int main(void) {
 	int server_fd, new_socket;
-	struct sockaddr_in address;
-	int addrlen = sizeof(address);
 	char buffer[BUFFER_SIZE] = {0};
 
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	struct sockaddr_in address = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = INADDR_ANY,
+		.sin_port = htons(PORT)
+	};
+	int addrlen = sizeof(address);
+
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(server_fd, 10) < 0) {
+		perror("listen failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+		perror("accept failed");
+		exit(EXIT_FAILURE);
 	}
 }
